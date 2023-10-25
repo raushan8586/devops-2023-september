21d84a51448981a7d17c3295eabfa164cadc7304
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2023-10-25 08:23:56 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,3 +17,31 @@ resource "aws_vpc" "intellipaat-vpc" {
   }
 }
 
+#create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.intellipaat-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+#create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.intellipaat-vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "private_subnet"
+  }
+}
+
+resource "aws_instance" "server-1" {
+  ami           = "ami-0287a05f0ef0e9d9a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server-1"
+  }
+}
