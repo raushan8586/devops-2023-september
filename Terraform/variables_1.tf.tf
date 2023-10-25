6fcffc85b9b4393dcdabd02b70f28a44031e039b
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2023-10-25 08:44:56 +0530

    Add files via upload

---
+++ Terraform/variables_1.tf.tf
@@ -0,0 +1,19 @@
+variable "cidr_block" {
+    type = list(string)
+    default = ["172.20.0.0/16","172.20.10.0/24"]
+}
+
+variable "ports" {
+    type = list(number)
+    default = [22,80,443,8080,8081,9000]
+}
+
+variable "ami" {
+    type = string
+    default = "ami-0b0f4c27376f8aa79"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
